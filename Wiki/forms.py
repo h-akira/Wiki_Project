75e70755fea1a6a23d5be7ac7404d47c8c46ce0c
h-akira <79441679+h-akira@users.noreply.github.com>
2023-09-21 00:28:08 +0900

    add js

--- Wiki/forms.py
+++ Wiki/forms.py
@@ -9,7 +9,7 @@ class PageForm(forms.ModelForm):
     widgets = {
       'text': MarkdownxWidget(
         attrs={
-          'style': 'width: 100%; hidden; height: 100%; overflow-y: hidden;',
+          'class': 'dynamic-textarea'
         }
       ),
       'title': forms.TextInput(
@@ -22,7 +22,6 @@ class PageForm(forms.ModelForm):
           'style': 'width: 100%;'
         }
       ),
-      # 'text': MarkdownxWidget(attrs={'rows': 25, 'style': 'width: 100%;'}),
     }
 
 
---
+++ static_local/js/textarea.js
@@ -0,0 +1,29 @@
+// Bardにより生成
+// ページ読み込み時に実行
+document.addEventListener('DOMContentLoaded', function() {
+    // classがdynamic-textareaであるtextareaを取得
+    const textareas = document.querySelectorAll('.dynamic-textarea');
+
+    // textareaの縦の大きさを調整
+    textareas.forEach(textarea => {
+        autoResizeTextArea(textarea);
+    });
+});
+
+// 入力時に実行
+document.addEventListener('input', function(event) {
+    // 対象要素がtextareaタグであり、かつclass属性に"dynamic-textarea"が含まれている場合
+    if (event.target.tagName.toLowerCase() === 'textarea' && event.target.classList.contains('dynamic-textarea')) {
+        // textareaの縦の大きさを調整
+        autoResizeTextArea(event.target);
+    }
+});
+
+// textareaの高さを調整する関数
+function autoResizeTextArea(textArea) {
+    // textareaのheight属性を取得
+    const height = textArea.scrollHeight + 'px';
+
+    // textareaのheight属性を設定
+    textArea.style.height = height;
+}
